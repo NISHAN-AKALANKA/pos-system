ed824676a3516324985df088f7ebc655f9858662
nishan akalanka <hdnakalanka@gmail.com>
2023-07-12 12:50:35 +0530

    setting up order service

--- pos-back-end/src/main/java/lk/ijse/dep10/pos/WebAppConfig.java
+++ pos-back-end/src/main/java/lk/ijse/dep10/pos/WebAppConfig.java
@@ -1,5 +1,9 @@
 package lk.ijse.dep10.pos;
 
+import lk.ijse.dep10.pos.api.CustomerController;
+import lk.ijse.dep10.pos.api.ItemController;
+import lk.ijse.dep10.pos.api.OrderController;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -7,5 +11,18 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 public class WebAppConfig {
 
+    @Bean
+    public CustomerController customerController(){
+        return new CustomerController();
+    }
 
+    @Bean
+    public ItemController itemController(){
+        return new ItemController();
+    }
+
+    @Bean
+    public OrderController orderController(){
+        return new OrderController();
+    }
 }
